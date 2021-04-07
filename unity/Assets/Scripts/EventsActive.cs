bdb9ba1e89c5329a83f3fcbdd381a5b9088367ad
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2021-04-06 21:53:18 -0500

    lost crypt and configure leap motion

---
+++ unity/Assets/Scripts/EventsActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsActive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void showMessage(){
+    	Debug.Log("ESTA EN RANGO");
+    }
+
+    public void noMessage(){
+
+    }
+}
